ea7ea087b7c39ed2c6ee769585ecd11b8da56366
[200~gowthamrathinasamy <gowtham101910@gmail.com>
2025-10-02 11:41:21 +0530

    nova strid

--- OpenBrowser.js
+++ OpenBrowser.js
@@ -1,10 +1,9 @@
 const { chromium } = require('playwright');
 
 (async () => {
-  console.log("Starting browser...");   // <-- to see output in terminal
   const browser = await chromium.launch({ headless: false }); 
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto('https://novastrid.com');
   console.log("Page title:", await page.title());
   await browser.close();
   console.log("Browser closed.");
